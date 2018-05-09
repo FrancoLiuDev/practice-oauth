f804e0aacbe1e02926a6e1cc87464c202015654f
Franco <franco.liu@leediancn.com>
2018-05-09 17:52:18 +0800

    update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,33 @@ import './store/index.js'
 
 Vue.config.productionTip = false
 
+window.fbAsyncInit = function() {
+    FB.init({
+        appId: '162108417151159',
+        cookie: true,
+        xfbml: true,
+        version: 'v2.9'
+    })
+    FB.AppEvents.logPageView()
+    console.log('fbAsyncInit')
+        // Get FB Login Status
+    FB.getLoginStatus(response => {
+        console.log('res', response) // 這裡可以得到 fb 回傳的結果
+    })
+    FB.login(function(response) {
+        //https://developers.facebook.com/docs/graph-api/reference/v2.2/user
+        console.log('login', response.authResponse.userID)
+        FB.api('/me?fields=id,name,email,cover,birthday,link,picture', function(info) {
+            console.log('/me', info)
+
+            // FB.api('/' + info.id + '/accounts?fields=is_business', 'GET', {}, function(accounts) {
+            //     console.log('/accounts', accounts)
+            //         // Insert your code here
+            // })
+        })
+    })
+}
+
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
