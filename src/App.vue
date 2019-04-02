a1ad33f70435198207987be0fd0a0b08e4f54462
franco <francoliu1979@gmail.com>
2019-04-02 23:01:22 +0800

    up

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
     <span>id:{{id}}</span>
     <div>
       <div id="fb-root"></div>
-      <div class="fb-customerchat" attribution="setup_tool"  page_id="285763282051548"></div>
-    
+      <div class="fb-customerchat" attribution="setup_tool" page_id="146864302897352"></div>
     </div>
     <router-view/>
   </div>
@@ -26,10 +25,10 @@ export default {
     window.fbAsyncInit = function() {
       FB.init({
         appId: "285763282051548",
-         status: true,
-      xfbml: true,
-      cookie: true,
-        version: 'v3.2'
+        status: true,
+        xfbml: true,
+        cookie: true,
+        version: "v3.2"
       });
 
       (function(d, s, id) {
@@ -38,7 +37,7 @@ export default {
         if (d.getElementById(id)) return;
         js = d.createElement(s);
         js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
         fjs.parentNode.insertBefore(js, fjs);
       })(document, "script", "facebook-jssdk");
 
@@ -52,15 +51,24 @@ export default {
         //https://developers.facebook.com/docs/graph-api/reference/v2.2/user
         console.log("login", response);
         self.id = response.authResponse.userID;
+
         FB.api("/me?fields=id,name,email,cover,birthday,link,picture", function(
           info
         ) {
           console.log("/me", info);
           self.info = info;
-          // FB.api('/' + info.id + '/accounts?fields=is_business', 'GET', {}, function(accounts) {
-          //     console.log('/accounts', accounts)
-          //         // Insert your code here
-          // })
+          FB.api(
+            "/" + info.id + "/accounts?fields=is_business",
+            "GET",
+            {},
+            function(accounts) {
+              console.log("/accounts", accounts);
+              // Insert your code here
+            }
+          );
+          FB.api("/" + info.id + "/ids_for_pages", function(response) {
+            console.log("ids_for_pages", response);
+          });
         });
       });
     };
